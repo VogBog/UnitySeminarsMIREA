30116c825319f583e47359d8a59dcc1d76dc74d9
VogBog <vogbog92@gmail.com>
2025-04-23 13:19:13 +0300

    feat: variant 3

--- Assets/Cube/CubeMovement.cs
+++ Assets/Cube/CubeMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace Cube
 {
-    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(Rigidbody), typeof(Animator))]
     public class CubeMovement : MonoBehaviour
     {
         [SerializeField] private float _jumpHeight;
@@ -12,11 +12,14 @@ namespace Cube
         
         private bool _isFalling;
         private Rigidbody _rb;
+        private Animator _animator;
         private float _jumpStrength;
 
         private void OnValidate()
         {
             _rb = GetComponent<Rigidbody>();
+            _animator = GetComponent<Animator>();
+            
             _jumpStrength = _rb.mass * Mathf.Sqrt(2 * Physics.gravity.magnitude * _jumpHeight);
         }
 
@@ -25,23 +28,30 @@ namespace Cube
             if (!_isFalling && Input.GetKeyDown(KeyCode.Space))
             {
                 _isFalling = true;
+                _animator.SetTrigger("Jump");
                 _rb.AddForce(_jumpStrength * Vector3.up, ForceMode.Impulse);
             }
-            
-            var x = Input.GetAxis("Horizontal");
-            var moveVec = x * _speed * Time.deltaTime * Vector3.right;
-            _rb.linearVelocity += moveVec;
         }
 
         private void FixedUpdate()
         {
+            var oldFalling = _isFalling;
             _isFalling = !IsOnGround();
+            if (oldFalling && !_isFalling)
+            {
+                _animator.SetTrigger("Ground");
+            }
+            
+            var x = Input.GetAxis("Horizontal");
+            var moveVec = x * _speed * Time.deltaTime * Vector3.right;
+            _rb.MovePosition(_rb.position + moveVec);
+            _animator.SetBool("Walking", x != 0);
         }
 
         private bool IsOnGround()
         {
             var ray = new Ray(transform.position, Vector3.down);
-            if (Physics.Raycast(ray, out var hit, .55f, LayerMask.GetMask("Ground")))
+            if (Physics.Raycast(ray, out var hit, .6f, LayerMask.GetMask("Ground")))
             {
                 return true;
             }
