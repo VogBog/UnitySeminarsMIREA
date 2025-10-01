a5d670fcf1d4221b13e384bf57926e2f1b752f84
VogBog <vogbog92@gmail.com>
2025-10-02 00:47:15 +0300

    feat: add prac5

---
+++ Assets/MyOwnImageDetector.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+namespace DefaultNamespace
+{
+    public class MyOwnImageDetector : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] _prefabs;
+        
+        private ARTrackedImageManager _trackedImageManager;
+        private readonly Dictionary<string, GameObject> _instances = new();
+
+        private void Awake()
+        {
+            _trackedImageManager = FindFirstObjectByType<ARTrackedImageManager>();
+            if(_trackedImageManager == null) throw new NullReferenceException("ARTrackedImageManager not found");
+
+            foreach (var prefab in _prefabs)
+            {
+                var instance = Instantiate(prefab, transform);
+                instance.SetActive(false);
+                _instances.Add(prefab.name, instance);
+            }
+        }
+
+        private void OnEnable()
+        {
+            _trackedImageManager.trackablesChanged.AddListener(ImageChanged);
+        }
+
+        private void OnDisable()
+        {
+            _trackedImageManager.trackablesChanged.RemoveListener(ImageChanged);
+        }
+
+        private void ImageChanged(ARTrackablesChangedEventArgs<ARTrackedImage> args)
+        {
+            foreach(var image in args.added)
+                UpdateInstanceByImage(image);
+            foreach(var image in args.updated)
+                UpdateInstanceByImage(image);
+
+            foreach (var (_, image) in args.removed)
+            {
+                if(_instances.TryGetValue(image.referenceImage.name, out var instance))
+                    instance.SetActive(false);
+            }
+        }
+
+        private void UpdateInstanceByImage(ARTrackedImage image)
+        {
+            if (string.IsNullOrEmpty(image.referenceImage.name))
+                return;
+            
+            if (!_instances.TryGetValue(image.referenceImage.name, out var instance))
+                return;
+            
+            instance.transform.SetPositionAndRotation(image.pose.position, image.pose.rotation);
+            instance.SetActive(true);
+        }
+    }
+}
