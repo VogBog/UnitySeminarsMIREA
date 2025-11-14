f61471429a8266d90231258f3a610e5e3d58e543
VogBog <vogbog92@gmail.com>
2025-11-14 23:57:04 +0300

    feat: add startTimer

--- Assets/Scripts/Game/GameUI.cs
+++ Assets/Scripts/Game/GameUI.cs
@@ -13,6 +13,9 @@ namespace Game
         [SerializeField] private TMP_Text _text;
         [SerializeField] private TMP_Text _speedText;
 
+        private bool _stopTimer = false;
+
+        public event Action<int> TimerChanged; 
         public event Action RaceStarted;
 
         private IEnumerator Start()
@@ -27,24 +30,46 @@ namespace Game
             
             _text.text = text;
 
-            yield return new WaitForSeconds(2f);
+            yield return new WaitForSeconds(1f);
+
+            if (_stopTimer) yield break;
+
+            yield return new WaitForSeconds(1f);
+
+            if (_stopTimer) yield break;
 
             for (int timer = 3; timer > 0; timer--)
             {
                 _text.text = timer.ToString();
+                TimerChanged?.Invoke(timer);
                 
                 yield return new WaitForSeconds(1f);
+
+                if (_stopTimer) yield break;
             }
 
-            _text.text = "GO";
-            RaceStarted?.Invoke();
-            
+            TimerChanged?.Invoke(0);
+            StopTimerAndUpdate(0);
+        }
+
+        private IEnumerator AfterStartRoutine()
+        {
             yield return new WaitForSeconds(1f);
             
             _text.gameObject.SetActive(false);
-
             var checkPoints = FindFirstObjectByType<CheckPointsHolder>();
             checkPoints.CarFinished += OnFinish;
+        }
+
+        public void StopTimerAndUpdate(int time)
+        {
+            _text.text = time.ToString();
+            if (time == 0)
+            {
+                _text.text = "GO";
+                RaceStarted?.Invoke();
+                StartCoroutine(AfterStartRoutine());
+            }
         }
 
         private void Update()
---
+++ Assets/Scripts/Game/LocalMultiplayer/LocalMultiplayerStartTimer.cs
@@ -0,0 +1,52 @@
+using Global;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Game.LocalMultiplayer
+{
+    public class LocalMultiplayerStartTimer : NetworkBehaviour
+    {
+        private GameUI _gameUI;
+        
+        private void Awake()
+        {
+            if (StaticParameters.NetworkType is not StaticParameters.NetworkTypes.LocalMultiplayer)
+            {
+                Destroy(this);
+                return;
+            }
+            
+            var players = FindObjectsByType<CarMovement>(FindObjectsSortMode.None);
+            foreach(var player in players)
+            {
+                if(!player.TryGetComponent(out NetworkObject networkObject) ||
+                   !networkObject.IsOwner)
+                    continue;
+
+                var gameUI = player.GetComponentInChildren<GameUI>();
+                InitializeGameUI(gameUI);
+                break;
+            }
+        }
+
+        private void InitializeGameUI(GameUI gameUI)
+        {
+            _gameUI = gameUI;
+
+            if (IsServer)
+                gameUI.TimerChanged += OnTimerChanged;
+        }
+
+        private void OnTimerChanged(int timer)
+        {
+            TimerChangedRpc(timer);
+        }
+
+        [Rpc(SendTo.Server, InvokePermission = RpcInvokePermission.Everyone)]
+        private void TimerChangedRpc(int timer)
+        {
+            if (IsServer) return;
+            _gameUI.StopTimerAndUpdate(timer);
+        }
+    }
+}
